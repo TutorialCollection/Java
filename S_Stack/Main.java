7ba281368c7b92e74c38a60fd86ccf4cfb6a9ae4
Sandro Gallo <5992750+sandrogallo@users.noreply.github.com>
2024-02-14 09:49:40 +0000

    fatta a lezione

--- S_Stack/Main.java
+++ S_Stack/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
     public static void main( String[] args ) {
         Stack<String> s = new Stack<>();
+        s.push("Milan");
+        s.push("Roma");
         s.push("Juve");
         s.push("Inter");
         System.out.println(s.pop());
