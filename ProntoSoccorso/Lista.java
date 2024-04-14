7f55c56680b7603995be8af08844012d112808b6
Sandro Gallo <5992750+sandrogallo@users.noreply.github.com>
2024-04-14 23:02:15 +0000

    Supermercato 1504

--- ProntoSoccorso/Lista.java
+++ ProntoSoccorso/Lista.java
@@ -37,24 +37,6 @@ public class Lista<T> {
         }
     }
 
-    public boolean addAfter( int pos, T v ) {
-        Nodo<T> n = new Nodo<>(v);
-        // aggiunge il nodo n solo dopo aver oltrepassato il nodo di indice pos
-        Nodo<T> tmp = root;
-        for (int i=0; i<pos; i++) {
-            tmp = root.getSuccessivo();
-        }
-
-        int i; Nodo npos=null;
-        for (i=0; i<pos; i++) {
-            if (iter.hasNext()) npos = iter.next();
-            else return false;
-        }
-        n.setSuccessivo(npos.getSuccessivo());
-        npos.setSuccessivo(n);
-        return true;
-    }
-    
     public String toString() {
         String s = "\nLIST BEGIN *************\n";
         Nodo<T> tmp = root;
